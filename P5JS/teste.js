88efa6f4749a2fdfddb6135a81079dbe72cef270
isabella <30278887+isabellabs@users.noreply.github.com>
2021-01-09 17:35:41 +0000

    Codigo com os casos de materiais separados

--- P5JS/teste.js
+++ P5JS/teste.js
@@ -8,7 +8,7 @@ let alignSlider, cohesionSlider, separationSlider;
 //timer fruta = 60
 //timer can = 8000
 //timer plastic = 18000
-//timer styrofoam = infinite
+//timer styrofoam = 40000000
 
 function preload() {
   plastic = loadImage('assets/plastic.jpeg');
@@ -20,7 +20,24 @@ function preload() {
 
 function setup() {
   createCanvas(1920, 1080);
+  //neutral - case 0
+  //image(neutral, 0, 0);
+
+  //fruit - case 1
+  //image(fruit, 0, 0);
+
+  //paperbag - case 2
+  //image(paperbag, 0, 0);
+
+  //can - case 3
+  //image(can, 0, 0);
+
+  //plastic - case 4
   image(plastic, 0, 0);
+
+  //styrofoam - case 5
+  //image(styrofoam, 0, 0);
+
   alignSlider = createSlider(0, 2, 1, 0.1);
   cohesionSlider = createSlider(0, 2, 1, 0.1);
   separationSlider = createSlider(0, 2, 1, 0.1);
@@ -30,6 +47,71 @@ function setup() {
 }
 
 function draw() { 
+
+  //fruta case 1
+  timer = 60
+  if (timer > 0) {
+    timer = timer-1;
+    print(timer);
+    var x1 = random(width);
+    var y1 = random(height);
+
+    var x2 = round(x1 + random(-150, 150));
+    var y2 = round(y1 + random(-150, 150));
+
+    var w = 200;
+    var h = 100;
+
+    set(x2, y2, get(x1, y1, w, h));
+    
+    for (let boid of flock) {
+      boid.edges();
+      boid.flock(flock);
+      boid.update();
+      boid.show();
+    }
+  }
+    
+  if (timer <= 0) {
+    textSize(20);
+    background(0);
+    Fill(255);
+    text("Did you know that fruit takes x years to decompose?", 400, height/2);
+  }
+
+  //paperbag - case 2
+  timer = 60
+  if (timer > 0) {
+    timer = timer-1;
+    print(timer);
+    var x1 = random(width);
+    var y1 = random(height);
+
+    var x2 = round(x1 + random(-160, 160));
+    var y2 = round(y1 + random(-160, 160));
+
+    var w = 210;
+    var h = 110;
+
+    set(x2, y2, get(x1, y1, w, h));
+    
+    for (let boid of flock) {
+      boid.edges();
+      boid.flock(flock);
+      boid.update();
+      boid.show();
+    }
+  }
+    
+  if (timer <= 0) {
+    textSize(20);
+    background(0);
+    Fill(255);
+    text("Did you know that paperbag takes x years to decompose?", 400, height/2);
+  }
+
+  //can - case 3
+  timer = 8000
   if (timer > 0) {
     timer = timer-1;
     print(timer);
@@ -56,7 +138,69 @@ function draw() {
     textSize(20);
     background(0);
     Fill(255);
-    text("Did you know that plastic takes 800 years to decompose?", 400, height/2);
+    text("Did you know that paperbag takes x years to decompose?", 400, height/2);
+  }
+
+  //plastic - case 4
+  timer = 18000
+  if (timer > 0) {
+    timer = timer-1;
+    print(timer);
+    var x1 = random(width);
+    var y1 = random(height);
+
+    var x2 = round(x1 + random(-5, 5));
+    var y2 = round(y1 + random(-5, 5));
+
+    var w = 150;
+    var h = 50;
+
+    set(x2, y2, get(x1, y1, w, h));
+    
+    for (let boid of flock) {
+      boid.edges();
+      boid.flock(flock);
+      boid.update();
+      boid.show();
+    }
+  }
+    
+  if (timer <= 0) {
+    textSize(20);
+    background(0);
+    Fill(255);
+    text("Did you know that paperbag takes x years to decompose?", 400, height/2);
+  }
+
+  //styrofoam - case 5
+  timer = 40000000
+  if (timer > 0) {
+    timer = timer-1;
+    print(timer);
+    var x1 = random(width);
+    var y1 = random(height);
+
+    var x2 = round(x1 + random(-1, 1));
+    var y2 = round(y1 + random(-1, 1));
+
+    var w = 100;
+    var h = 25;
+
+    set(x2, y2, get(x1, y1, w, h));
+    
+    for (let boid of flock) {
+      boid.edges();
+      boid.flock(flock);
+      boid.update();
+      boid.show();
+    }
+  }
+    
+  if (timer <= 0) {
+    textSize(20);
+    background(0);
+    Fill(255);
+    text("Did you know that styrofoam takes 1000000 years to decompose?", 400, height/2);
   }
 }
 
